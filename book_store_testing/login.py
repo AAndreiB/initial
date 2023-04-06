6efc3817c4dd11fddb5366e6a06709c68f4c66e4
AAndreiB <reisler@mail.ru>
2023-04-06 16:48:36 +0300

    logn.py Login user

--- book_store_testing/login.py
+++ book_store_testing/login.py
@@ -10,14 +10,13 @@ driver.maximize_window()
 myAccount = driver.find_element_by_xpath('//a[text()="My Account"]') # using different types of selectors
 myAccount.click()
 
-# register user
-email = driver.find_element_by_css_selector("form.register input[type='email']")
+# login user
+email = driver.find_element_by_css_selector("form.login input[type='text']")
 email.send_keys('qa.automat@yandex.ru')
-password = driver.find_element_by_css_selector("form.register input[type='password']")
+password = driver.find_element_by_css_selector("form.login input[type='password']")
 password.send_keys('secret_code')
-time.sleep(1)
-register = driver.find_element_by_css_selector("[value='Register']")
-register.click()
+submit = driver.find_element_by_css_selector("form.login input[type='submit']")
+submit.click()
 
 time.sleep(10)
 driver.quit()
